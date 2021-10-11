31efbbf1029e34f612747f5a35fe79d5d8ea4e64
Won Seok Kim <76798309+dnjstjr245@users.noreply.github.com>
2021-10-11 19:17:00 +0900

    feat: update MainActivity UI

--- app/src/main/java/com/wonseok/ddoc_doc/MainActivity.kt
+++ app/src/main/java/com/wonseok/ddoc_doc/MainActivity.kt
@@ -3,15 +3,21 @@ package com.wonseok.ddoc_doc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.wonseok.ddoc_doc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     //뒤로가기 연속 클릭 대기 시간
     private var backPressedTime : Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        Glide.with(this).load(R.drawable.main_animation_icon).into(binding.mainAnimationGifImageView)
     }
 
     override fun onBackPressed() {
@@ -22,9 +28,8 @@ class MainActivity : AppCompatActivity() {
             android.os.Process.killProcess(android.os.Process.myPid())
             return
         }
-
         // 처음 클릭 메시지
-        Toast.makeText(this, "'뒤로' 버튼을 한번 더 누르시면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "앱을 종료합니다.", Toast.LENGTH_SHORT).show()
         backPressedTime = System.currentTimeMillis()
     }
 }
